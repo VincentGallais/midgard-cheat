601741da06e809bbbd378c0b8d8692c877567892
Vincent Gallais <vincent.gallais@52-entertainment.com>
2025-09-15 16:37:17 +0200

    update bridge-commons

--- src/detection.ts
+++ src/detection.ts
@@ -1,9 +1,11 @@
 import fs from 'fs/promises'
-import { constants } from 'bridge-commons/core'
-import { Game } from 'bridge-commons/core/classes'
 import * as utils from './utils'
+import { ArgineInterface, Card, Game, Player } from 'bridge-commons/core/classes'
 
-analyze()
+// analyze()
+
+const argineInterface = new ArgineInterface()
+argineInterface.setArgineUrl('http://localhost:3000')
 
 export async function analyze() {
   const raw = await fs.readFile('./src/mockData.json', 'utf-8')
@@ -12,7 +14,7 @@ export async function analyze() {
   for (const inputGame of games.slice(0, 5)) {
     const game = utils.parseToArgine(inputGame)
 
-    if (game.contract!.leader == constants.PLAYER_S) {
+    if (Player.isSamePlayer(game.contract?.leader, Player.S)) {
       await scanLead(game)
     }
 
@@ -27,12 +29,12 @@ async function scanLead(game: Game) {
     const query = { ...game.toArgine() }
     query.game.cards = ''
 
-    const argineLead = await utils.getCard(query)
+    const argineLead = Card.fromName(await argineInterface.getCard(query), Player.S)
     const playerLead = game.playedCards.at(0)!
 
-    const leadEval = (await utils.getEval(query))[0]
+    const leadEval = (await argineInterface.getEval(query, Player.S))[0]
 
-    const playerCards = game.distribution.getByPlayer(constants.PLAYER_S)
+    const playerCards = game.distribution.getByPlayer(Player.S)
 
     // Index of eval cards is reversed
     const argineLeadIdx = 12 - playerCards.indexOf(argineLead)
@@ -56,24 +58,24 @@ async function scanBids(game: Game) {
   const query = game.toArgine()
 
   for (const [idx, bid] of game.bidList.entries()) {
-    if (bid.player === constants.PLAYER_S) {
+    if (Player.isSamePlayer(bid.player, Player.S)) {
       query.game.bids = game.bidList.toArgineString().slice(0, (idx + 1) * 2)
 
-      const bidInfo = await utils.getBidInfo(query)
+      const bidInfo = await argineInterface.getBidInfo(query)
 
-      for (const suit of constants.SUITS) {
-        const playerSuitSize = game.distribution.getBySuit(suit).getByPlayer(constants.PLAYER_S).size
-        const suitInfo = bidInfo.getBySuit(suit)
+      // for (const suit of constants.SUITS) {
+      //   const playerSuitSize = game.distribution.getBySuit(suit).getByPlayer(Player.S).length
+      //   const suitInfo = bidInfo.getBySuit(suit)
 
-        if (playerSuitSize < suitInfo.min - TOLERANCE.card || playerSuitSize > suitInfo.max + TOLERANCE.card) {
-          console.log(`${utils.suitNames[suit]}: ${playerSuitSize} cartes (attendu: ${suitInfo.min}-${suitInfo.max})`)
-        }
-      }
+      //   if (playerSuitSize < suitInfo.min - TOLERANCE.card || playerSuitSize > suitInfo.max + TOLERANCE.card) {
+      //     console.log(`${utils.suitNames[suit]}: ${playerSuitSize} cartes (attendu: ${suitInfo.min}-${suitInfo.max})`)
+      //   }
+      // }
 
-      const playerHcp = game.distribution.getByPlayer(constants.PLAYER_S).hcp
-      if (playerHcp < bidInfo.hcp.min - TOLERANCE.hcp || playerHcp > bidInfo.hcp.max + TOLERANCE.hcp) {
-        console.log(`HCP: ${playerHcp} (attendu: ${bidInfo.hcp.min}-${bidInfo.hcp.max})`)
-      }
+      // const playerHcp = game.distribution.getByPlayer(Player.S).hcp
+      // if (playerHcp < bidInfo.hcp.min - TOLERANCE.hcp || playerHcp > bidInfo.hcp.max + TOLERANCE.hcp) {
+      //   console.log(`HCP: ${playerHcp} (attendu: ${bidInfo.hcp.min}-${bidInfo.hcp.max})`)
+      // }
     }
   }
 }
@@ -83,22 +85,25 @@ async function scanCards(game: Game) {
   const query = { ...game.toArgine() }
 
   const cardsEval = {
-    south: await utils.getEval(query, constants.PLAYER_S),
-    north: await utils.getEval(query, constants.PLAYER_N)
+    south: await argineInterface.getEval(query, Player.S),
+    north: await argineInterface.getEval(query, Player.N)
   }
 
   for (const [cardIdx, card] of game.playedCards.entries()) {
     // We don't analyze cards with idx > 48 (last trick, player don't have any choice)
-    if (cardIdx < 48 && (card.player === constants.PLAYER_S || (card.player === constants.PLAYER_N && game.contract?.declarer === constants.PLAYER_S))) {
+    if (
+      cardIdx < 48 &&
+      (Player.isSamePlayer(card.player, Player.S) || (Player.isSamePlayer(card.player, Player.N) && Player.isSamePlayer(game.contract?.declarer, Player.S)))
+    ) {
       query.game.cards = game.playedCards.toArgineString().slice(0, cardIdx * 2)
 
-      const playerCards = game.distribution.getByPlayer(constants.PLAYER_S)
-      const argineCard = await utils.getCard(query)
+      const playerCards = game.distribution.getByPlayer(Player.S)
+      const argineCard = Card.fromName(await argineInterface.getCard(query), Player.S)
 
       const argineCardIdx = 12 - playerCards.indexOf(argineCard)
       const playerLeadIdx = 12 - playerCards.indexOf(card)
 
-      const playerEval = card.player === constants.PLAYER_S ? cardsEval.south : cardsEval.north
+      const playerEval = Player.isSamePlayer(card.player, Player.S) ? cardsEval.south : cardsEval.north
       const argineCardEval = playerEval[cardIdx][argineCardIdx]
       const playerCardEval = playerEval[cardIdx][playerLeadIdx]
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,107 +1,5 @@
-import { Card, Game } from 'bridge-commons/core/classes'
-import { ArgineInterface, QueryTypes } from 'midgard-commons/lib/argine-interface.js'
-import { constants } from 'bridge-commons/core'
-
-const argineInterface = new ArgineInterface()
-
-interface SuitInfo {
-  min: number
-  max: number
-}
-
-class BidInfo {
-  public c: SuitInfo
-  public d: SuitInfo
-  public h: SuitInfo
-  public s: SuitInfo
-  public hcp: SuitInfo
-  public meaning: string
-  public forcing: number
-
-  protected constructor(c: SuitInfo, d: SuitInfo, h: SuitInfo, s: SuitInfo, hcp: SuitInfo, meaning: string, forcing: number) {
-    this.c = c
-    this.d = d
-    this.h = h
-    this.s = s
-    this.hcp = hcp
-    this.meaning = meaning
-    this.forcing = forcing
-  }
-
-  public getBySuit(suit: any): SuitInfo {
-    switch (suit) {
-      case constants.SUIT_C:
-        return this.c
-      case constants.SUIT_D:
-        return this.d
-      case constants.SUIT_H:
-        return this.h
-      case constants.SUIT_S:
-        return this.s
-      default:
-        throw new Error(`Unknown suit: ${suit}`)
-    }
-  }
-
-  static fromArgineData(splitedValue: string[]): BidInfo {
-    return new BidInfo(
-      { min: Number(splitedValue[0]), max: Number(splitedValue[1]) }, // club
-      { min: Number(splitedValue[2]), max: Number(splitedValue[3]) }, // diamond
-      { min: Number(splitedValue[4]), max: Number(splitedValue[5]) }, // heart
-      { min: Number(splitedValue[6]), max: Number(splitedValue[7]) }, // spade
-      { min: Number(splitedValue[10]), max: Number(splitedValue[11]) }, // hcp
-      splitedValue[12], // meaning
-      Number(splitedValue[13]) // forcing
-    )
-  }
-}
-
-export const suitNames = {
-  [constants.SUIT_C]: 'Trèfles',
-  [constants.SUIT_D]: 'Carreaux',
-  [constants.SUIT_H]: 'Cœurs',
-  [constants.SUIT_S]: 'Piques'
-}
-
-// TODO : Mettre PlayerPosition
-function getPlayerMask(player: any) {
-  switch (player) {
-    case constants.PLAYER_S:
-      return 1
-    case constants.PLAYER_W:
-      return 2
-    case constants.PLAYER_N:
-      return 4
-    case constants.PLAYER_E:
-      return 8
-    default:
-      throw new Error(`Unknown player position: ${player}`)
-  }
-}
-
-// TODO : Créer un type ArgineQuery dans la lib
-export async function getCard(query: any) {
-  const { data } = await argineInterface.callArgine(query, QueryTypes.Card)
-  return Card.fromName(data.value.slice(0, 2), constants.PLAYER_S)
-}
-
-export async function getEval(query: any, player = constants.PLAYER_S) {
-  const { data } = await argineInterface.callArgine({ ...query, maskHand: getPlayerMask(player) }, QueryTypes.Eval)
-  const splitData = data.value.split(';')
-  const result: string[][] = []
-
-  for (let i = 0; i < splitData.length; i += 13) {
-    result.push(splitData.slice(i, i + 13))
-  }
-
-  return result
-}
-
-export async function getBidInfo(query: any): Promise<BidInfo> {
-  const { data } = await argineInterface.callArgine(query, QueryTypes.BidInfo)
-  const splitedValue = data.value.split(';')
-  return BidInfo.fromArgineData(splitedValue)
-}
+import { Game } from 'bridge-commons/core/classes'
+import { DEFAULT_CONVENTIONS, DEFAULT_OPTIONS } from 'bridge-commons/core/constants'
 
 function splitData(input: string) {
   return input
@@ -122,8 +20,8 @@ export function parseToArgine(game: any) {
       cards: splitData(game.ST_cards),
       bids: splitData(game.ST_bids)
     },
-    conventions: { NS: constants.DEFAULT_CONVENTIONS, EW: constants.DEFAULT_CONVENTIONS },
-    options: { argineConfidence: 0, scoringType: 0 }
+    conventions: { NS: DEFAULT_CONVENTIONS, EW: DEFAULT_CONVENTIONS },
+    options: DEFAULT_OPTIONS
   }
 
   return Game.fromArgineData(argineFormat)
